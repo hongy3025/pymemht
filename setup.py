dba27f59253f0f39ad4554bdb147be69159ca8ed
hongy <hongy3025@163.com>
2016-02-16 16:59:49 +0800

    build script

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
+ext_name = 'memht'
+# extra_compile_args = ['-Wno-unused-function', '-Wno-unneeded-internal-declaration']
+extra_compile_args = []
+
 try:
     from Cython.Build import cythonize
     ext_modules = cythonize([Extension(
-        'memht',
+        ext_name,
         sources=['memht.pyx', 'hashtable.cpp'],
-        extra_compile_args=['-Wno-unused-function', '-Wno-unneeded-internal-declaration']
+        extra_compile_args=extra_compile_args
     )])
 except ImportError:
     ext_modules = [Extension(
-        'memht',
-        sources=['memht.cpp'],
-        extra_compile_args=['-Wno-unused-function', '-Wno-unneeded-internal-declaration']
+        ext_name,
+        sources=['memht.cpp', 'hashtable.cpp'],
+        extra_compile_args=extra_compile_args
     )]
 
 setup(version='0.1',
