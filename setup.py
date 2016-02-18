0a37d3b1da58f094742dea983cca096accbb7378
hongy <hongy3025@163.com>
2016-02-18 10:03:14 +0800

    条件编译

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
 from distutils.core import setup
 from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
 
 ext_name = 'memht'
-extra_compile_args = ['-Wno-unused-function', '-Wno-unneeded-internal-declaration']
+
+extra_compile_args = {
+    'msvc': ['/wd4146'],
+    'gcc': ['-Wno-unused-function', '-Wno-unneeded-internal-declaration'],
+}
 
 try:
     from Cython.Build import cythonize
     ext_modules = cythonize([Extension(
         ext_name,
         sources=['memht.pyx', 'hashtable.cpp'],
-        extra_compile_args=extra_compile_args
     )])
 except ImportError:
     ext_modules = [Extension(
         ext_name,
         sources=['memht.cpp', 'hashtable.cpp'],
-        extra_compile_args=extra_compile_args
     )]
+
+class BuildExtSubclass(build_ext):
+    def build_extensions(self):
+        c = self.compiler.compiler_type
+        extra_copts = extra_compile_args.get(c, 'gcc')
+        for e in self.extensions:
+            e.extra_compile_args += extra_copts
+        build_ext.build_extensions(self)
 
 setup(version='0.1',
       name='memht',
@@ -24,4 +35,5 @@ setup(version='0.1',
       packages=['memht'],
       author='HongYing',
       author_email='hongy3025@163.com',
+      cmdclass={'build_ext': BuildExtSubclass},
       description='inplace memory hashtable')
